ec4bc13a2c9b8ab3e73da2f5a58e8add961cdae0
joeGdev <joe.godwin2001@gmail.com>
2025-11-06 21:12:42 +0000

    Add files via upload
    Styled version

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
+
 	const form = document.getElementById('l_form');
-	const hidden = document.getElementById('l_summon_form');
-	hidden.addEventListener('click', function (e) {
+	const table = document.querySelector('#l_table tbody');
+
+
+	const summonForm = document.getElementById('l_summon_form');
+	summonForm.addEventListener('click', function (e) {
 		e.preventDefault();
-		
+
 		form.classList.toggle('hidden');
 	});
 
-	const submit = document.getElementById('l_submit');
-	submit.addEventListener('click', function (e) {
+	const submitForm = document.getElementById('l_submit');
+	submitForm.addEventListener('click', function (e) {
 		e.preventDefault();
 
 		const name = document.getElementById('l_name').value;
@@ -16,18 +20,19 @@ document.addEventListener('DOMContentLoaded', function () {
 		const phone = document.getElementById('l_phone').value;
 		const department = document.getElementById('l_depart').value;
 
-		const table = document.querySelector('#l_table tbody');
-
-		const newRow = document.createElement('tr');
-		newRow.innerHTML = `
+		const createTr = document.createElement('tr');
+		createTr.innerHTML = `
 			<td>${name}</td>
 			<td>${role}</td>
 			<td>${phone}</td>
 			<td>${department}</td>
+			<td><a href="" class="btn btn-danger" id="l_delete">Delete</a></td>
 		`;
-		table.appendChild(newRow);
+
+		table.appendChild(createTr);
 
 		form.reset();
 	});
+
 
 });
